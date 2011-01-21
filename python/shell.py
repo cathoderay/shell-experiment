306ab391d4121dce56d968696671adabf2e1de18
Ronald Kaiser <raios.catodicos@gmail.com>
2011-01-21 02:29:34 -0200

    a simple python repl =)

---
+++ python/shell.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+    File: shell.py
+    Description: a simple python REPL
+"""
+
+__author__ = "raios at catodicos dot gmail dot com"
+
+
+print """python-shell -- version 0.0.1"""
+while True:
+    inp = raw_input("¬¬ ")
+    try:
+        exec(inp)
+    except Exception, e:
+        print e
+
